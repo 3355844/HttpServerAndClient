65133424e80eb08be3e5ce86ed4f1a05baa379b7
3355844 <3355844@gmail.com>
2017-01-21 14:48:42 +0200

    Create Server and answer on LocalHost 9999

--- src/MainServerRunner.java
+++ src/MainServerRunner.java
@@ -1,8 +1,23 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Created by 33558 on 20.01.2017.
  */
 public class MainServerRunner {
     public static void main(String[] args) {
+        ServerSocket serverSocket;
+        try {
+            serverSocket = new ServerSocket(9999);
+            while (true){
+                Socket socket = serverSocket.accept();
+                System.err.println("Client accepted");
+                new Thread(new SocketProcessor(socket)).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
